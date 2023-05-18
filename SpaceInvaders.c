4975e7c85f6a60ebf39294e94654cf2032c446ab
peter0124 <82339024+peter0124@users.noreply.github.com>
2023-05-19 01:17:29 +0300

    Update SpaceInvaders.c

--- SpaceInvaders.c
+++ SpaceInvaders.c
@@ -135,16 +135,15 @@ void Timer2A_Handler(void)
 }
 
 void PortF_Init(void)
-{
-  volatile unsigned long delay;
+{ 
+
   SYSCTL_RCGC2_R |= 0x00000020;   // 1) F clock
   delay = SYSCTL_RCGC2_R;         // delay
-  GPIO_PORTF_LOCK_R = 0x4C4F434B; // 2) unlock PortF PF0
-  GPIO_PORTF_CR_R = 0x1F;         // allow changes to PF4-0
-  GPIO_PORTF_AMSEL_R = 0x00;      // 3) disable analog function
-  GPIO_PORTF_PCTL_R = 0x00000000; // 4) GPIO clear bit PCTL
-  GPIO_PORTF_DIR_R = 0x0E;        // 5) PF4,PF0 input, PF3,PF2,PF1 output
-  GPIO_PORTF_AFSEL_R = 0x00;      // 6) no alternate function
-  GPIO_PORTF_PUR_R = 0x11;        // enable pullup resistors on PF4,PF0
-  GPIO_PORTF_DEN_R = 0x1F;        // 7) enable digital pins PF4-PF0
+	GPIO_vidUnlock(GPIOF);
+  GPIOF_CR_R = 0x1F;         // allow changes to PF4-0
+  GPIOF_AMSEL_R = 0x00;      // 3) disable analog function
+  GPIOF_PCTL_R = 0x00000000; // 4) GPIO clear bit PCTL
+  GPIOF_AFSEL_R = 0x00;      // 6) no alternate function
+  NVIC_PRI7_R = (NVIC_PRI7_R & 0xff00ffff) | 0x00A00000;
+  EnableInterrupts();
 }
